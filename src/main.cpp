674f592aa00ccfefd9c5a403e08ee6937a9ad88c
Dipod <dipod.dopid@gmail.com>
2020-11-19 14:41:12 +0300

    Task "Bad macro" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,21 @@
-#include "airline_ticket.h"
 #include "test_runner.h"
 
-#include <tuple>
-#include <istream>
+#include <ostream>
 using namespace std;
 
-ostream& operator<<(ostream &os, const Date &s) {
-	os << s.year << "-" << s.month << "-" << s.day;
-	return os;
-}
+#define PRINT_VALUES(out, x, y) PrintValues(out, x, y) // No need macro at all in this task. Template function will be better.
 
-ostream& operator<<(ostream &os, const Time &s) {
-	os << s.hours << ":" << s.minutes;
-	return os;
-}
-
-istream& operator>>(istream &is, Date &s) {
-	char separator;
-	is >> s.year >> separator >> s.month >> separator >> s.day;
-	return is;
-}
-
-istream& operator>>(istream &is, Time &s) {
-	char separator;
-	is >> s.hours >> separator >> s.minutes;
-	return is;
-}
-
-bool operator<(const Date &lhs, const Date &rhs) {
-	return tie(lhs.year, lhs.month, lhs.day) < tie(rhs.year, rhs.month, rhs.day);
-}
-
-bool operator<(const Time &lhs, const Time &rhs) {
-	return tie(lhs.hours, lhs.minutes) < tie(rhs.hours, rhs.minutes);
-}
-
-bool operator==(const Date &lhs, const Date &rhs) {
-	return tie(lhs.year, lhs.month, lhs.day)
-			== tie(rhs.year, rhs.month, rhs.day);
-}
-
-bool operator==(const Time &lhs, const Time &rhs) {
-	return tie(lhs.hours, lhs.minutes) == tie(rhs.hours, rhs.minutes);
-}
-
-#define UPDATE_FIELD(ticket, field, values)	\
-	{										\
-	  auto it = values.find(#field);		\
-	  if (it != values.end()) {				\
-	    istringstream is(it->second);		\
-	    is >> ticket.field;					\
-	  }										\
-	}
-
-void TestUpdate() {
-	AirlineTicket t;
-	t.price = 0;
-
-	const map<string, string> updates1 = { { "departure_date", "2018-2-28" }, {
-			"departure_time", "17:40" }, };
-	UPDATE_FIELD(t, departure_date, updates1);
-	UPDATE_FIELD(t, departure_time, updates1);
-	UPDATE_FIELD(t, price, updates1);
-
-	ASSERT_EQUAL(t.departure_date, (Date { 2018, 2, 28 }));
-	ASSERT_EQUAL(t.departure_time, (Time { 17, 40 }));
-	ASSERT_EQUAL(t.price, 0);
-
-	const map<string, string> updates2 = { { "price", "12550" }, {
-			"arrival_time", "20:33" }, };
-	UPDATE_FIELD(t, departure_date, updates2);
-	UPDATE_FIELD(t, departure_time, updates2);
-	UPDATE_FIELD(t, arrival_time, updates2);
-	UPDATE_FIELD(t, price, updates2);
-
-	// updates2 не содержит ключей "departure_date" и "departure_time", поэтому
-	// значения этих полей не должны измениться
-	ASSERT_EQUAL(t.departure_date, (Date { 2018, 2, 28 }));
-	ASSERT_EQUAL(t.departure_time, (Time { 17, 40 }));
-	ASSERT_EQUAL(t.price, 12550);
-	ASSERT_EQUAL(t.arrival_time, (Time { 20, 33 }));
+template<typename T, typename R>
+void PrintValues(ostream &out, const T &x, const R &y) {
+	out << x << endl;
+	out << y << endl;
 }
 
 int main() {
 	TestRunner tr;
-	RUN_TEST(tr, TestUpdate);
+	tr.RunTest([] {
+		ostringstream output;
+		PRINT_VALUES(output, 5, "red belt");
+		ASSERT_EQUAL(output.str(), "5\nred belt\n");
+	}, "PRINT_VALUES usage example");
 }

9655a8b6b6a279f9b0308f34806a5155d2b8bd0c
Dipod <dipod.dopid@gmail.com>
2020-11-19 19:36:22 +0300

    Task "Table" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,59 @@
-#include <string>
-#include <vector>
+#include "test_runner.h"
+#include <utility>
+
 using namespace std;
 
-#define CONCAT_2(x, y) x##y
-#define CONCAT_1(x, y) CONCAT_2(x, y)
-#define UNIQ_ID CONCAT_1(id_, __LINE__)
+template<typename T>
+class Table {
+public:
+	Table(const size_t &row, const size_t &col) {
+		Resize(row, col);
+	}
+
+	vector<T>& operator[](const size_t &row) const {
+		return table[row];
+	}
+
+	vector<T>& operator[](const size_t &row) {
+		return table[row];
+	}
+
+	void Resize(const size_t &row, const size_t &col) {
+		table.resize(row);
+		for (auto &table_row : table) {
+			table_row.resize(col);
+		}
+	}
+
+	pair<size_t, size_t> Size() const {
+		size_t rows = table.size(), cols = 0;
+		if (rows == 0u) {
+			return make_pair(rows, cols);
+		}
+		cols = table[0].size();
+		if (cols == 0u) {
+			rows = 0;
+		}
+		return make_pair(rows, cols);
+	}
+
+private:
+	vector<vector<T>> table;
+};
+
+void TestTable() {
+	Table<int> t(1, 1);
+	ASSERT_EQUAL(t.Size().first, 1u);
+	ASSERT_EQUAL(t.Size().second, 1u);
+	t[0][0] = 42;
+	ASSERT_EQUAL(t[0][0], 42);
+	t.Resize(3, 4);
+	ASSERT_EQUAL(t.Size().first, 3u);
+	ASSERT_EQUAL(t.Size().second, 4u);
+}
 
 int main() {
-  int UNIQ_ID = 0;
-  string UNIQ_ID = "hello";
-  vector<string> UNIQ_ID = {"hello", "world"};
-  vector<int> UNIQ_ID = {1, 2, 3, 4};
+	TestRunner tr;
+	RUN_TEST(tr, TestTable);
+	return 0;
 }

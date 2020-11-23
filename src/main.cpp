676c99f0b7af81f29c02f5898b7ed0b80d9319c4
Dipod <dipod.dopid@gmail.com>
2020-11-23 20:43:15 +0300

    Task "Express trains" solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,75 @@
+#include "test_runner.h"
+#include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <map>
 #include <string>
-#include <vector>
-#include <sstream>
 #include <set>
 
 using namespace std;
 
-class Learner {
-private:
-	set<string> dict;
-
+class RouteManager {
 public:
-	int Learn(const vector<string> &words) {
-		int newWords = 0;
-		for (const auto &word : words) {
-			if (dict.find(word) == dict.end()) {
-				++newWords;
-				dict.insert(word);
+	void AddRoute(int start, int finish) {
+		reachable_lists_[start].insert(finish);
+		reachable_lists_[finish].insert(start);
+	}
+	int FindNearestFinish(int start, int finish) const {
+		int result = abs(start - finish);
+		if (reachable_lists_.find(start) == reachable_lists_.end()) {
+			return result;
+		}
+		const set<int> &reachable_stations = reachable_lists_.at(start);
+		if (!reachable_stations.empty()) {
+			auto lower_bound = reachable_stations.lower_bound(finish);
+			if(lower_bound != reachable_stations.end()){
+				result = min(result, abs(finish - *lower_bound));
+			}
+			if(lower_bound != reachable_stations.begin()){
+				result = min(result, abs(finish - *prev(lower_bound)));
 			}
 		}
-		return newWords;
+		return result;
 	}
-
-	vector<string> KnownWords() {
-		return {dict.begin(), dict.end()};
-	}
+private:
+	map<int, set<int>> reachable_lists_;
 };
 
+void TestRouteManager() {
+
+	RouteManager routes;
+
+	routes.AddRoute(-2, 5);
+	routes.AddRoute(10, 4);
+	routes.AddRoute(5, 8);
+
+	ASSERT_EQUAL(routes.FindNearestFinish(4, 10), 0);
+	ASSERT_EQUAL(routes.FindNearestFinish(4, -2), 6);
+	ASSERT_EQUAL(routes.FindNearestFinish(5, 0), 2);
+	ASSERT_EQUAL(routes.FindNearestFinish(5, 100), 92);
+}
+
 int main() {
-	Learner learner;
-	string line;
-	while (getline(cin, line)) {
-		vector<string> words;
-		stringstream ss(line);
-		string word;
-		while (ss >> word) {
-			words.push_back(word);
+
+	TestRunner tr;
+	RUN_TEST(tr, TestRouteManager);
+
+	RouteManager routes;
+
+	int query_count;
+	cin >> query_count;
+
+	for (int query_id = 0; query_id < query_count; ++query_id) {
+		string query_type;
+		cin >> query_type;
+		int start, finish;
+		cin >> start >> finish;
+		if (query_type == "ADD") {
+			routes.AddRoute(start, finish);
+		} else if (query_type == "GO") {
+			cout << routes.FindNearestFinish(start, finish) << "\n";
 		}
-		cout << learner.Learn(words) << "\n";
 	}
-	cout << "=== known words ===\n";
-	for (auto word : learner.KnownWords()) {
-		cout << word << "\n";
-	}
+
+	return 0;
 }
